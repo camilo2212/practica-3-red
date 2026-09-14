92061308c8dcd6290140cd3564591f59db9ae287
camilo2212 <115837272+camilo2212@users.noreply.github.com>
2026-09-14 17:00:25 -0500

    fix: descarte default

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 # variables.tf
 variable "proyecto"     { type = string }
 variable "prefijo"      { type = string }
-variable "region"       { type = string  = "us-central1" }
-variable "cidr_publica" { type = string = "10.10.1.0/24" }
+variable "region"       { type = string }
+variable "cidr_publica" { type = string }
