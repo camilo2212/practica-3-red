d0a51a65ea0688098ce515945d2c08336e7e8c59
MaferCorzo <mariafernandacoca@ufps.edu.co>
2026-09-19 09:59:39 -0500

    feat(practica-mafer): recurso de la instancia

--- practica-mafer/main.tf
+++ practica-mafer/main.tf
@@ -23,3 +23,25 @@ resource "google_compute_subnetwork" "publica" {
   region        = var.region
   network       = google_compute_network.vpc.id
 }
+
+resource "google_compute_instance" "app" {
+  name         = "${var.prefijo}-app"
+  machine_type = var.tipo_maquina
+  zone         = var.zona
+  tags         = ["http-server"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    # la máquina debe quedar en tu subred, no en la default
+    subnetwork = google_compute_subnetwork.publica.id
+    # un bloque vacío aquí otorga una IP pública efímera
+    access_config {}
+  }
+
+  metadata_startup_script = file("${path.module}/arranque.sh")
+}
--- practica-mafer/variables.tf
+++ practica-mafer/variables.tf
@@ -1,8 +1,8 @@
 # variables.tf
-variable "proyecto"     { type = string }
-variable "prefijo"      { type = string }
-variable "region"       { type = string }
-variable "cidr_publica" { type = string }
+variable "proyecto"     { default = "maquinavirtual-507221" }
+variable "prefijo"      { default = "mafer-vpc" }
+variable "region"       { default = "us-central1" }
+variable "cidr_publica" { default = "10.10.1.0/24" }
 variable "zona"          { default = "us-central1-a" }
 variable "tipo_maquina"  { default = "e2-micro" }
 variable "cidr_privada"  { default = "10.10.2.0/24" }
