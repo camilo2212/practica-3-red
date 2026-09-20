decd00ab9acec636e5de4ecf7977350f63d8728c
camilo2212 <juancamilouribemendoza@gmail.com>
2026-09-20 17:44:27 -0500

    fix:output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "subred_publica" {
   value       = google_compute_subnetwork.publica.self_link
   description = "identificador completo de la subred de aplicación"
 }
+
+output "ip_publica_app" {
+  value       = google_compute_instance.app.network_interface[0].access_config[0].nat_ip
+  description = "IP pública de la máquina de aplicación"
+}
--- practica-mafer/outputs.tf
+++ practica-mafer/outputs.tf
@@ -1,14 +1,10 @@
+# outputs.tf
 output "red" {
   value       = google_compute_network.vpc.name
-  description = "nombre de la VPC creada"
+  description = "Nombre de la VPC creada"
 }
 
 output "subred_publica" {
   value       = google_compute_subnetwork.publica.self_link
-  description = "identificador completo de la subred de aplicación"
-}
-
-output "ip_publica_app" {
-  value       = google_compute_instance.app.network_interface[0].access_config[0].nat_ip
-  description = "IP pública de la máquina de aplicación"
+  description = "Identificador completo de la subred de aplicación"
 }
