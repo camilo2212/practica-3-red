ad41c3e55e41fb73c9d17536bc911dc06a38555d
camilo2212 <juancamilouribemendoza@gmail.com>
2026-09-20 18:21:26 -0500

    p6

--- practica-mafer/outputs.tf
+++ practica-mafer/outputs.tf
@@ -1,10 +1,19 @@
-# outputs.tf
 output "red" {
   value       = google_compute_network.vpc.name
-  description = "Nombre de la VPC creada"
+  description = "nombre de la VPC creada"
 }
 
 output "subred_publica" {
   value       = google_compute_subnetwork.publica.self_link
-  description = "Identificador completo de la subred de aplicación"
+  description = "identificador completo de la subred de aplicación"
+}
+
+output "ip_publica_app" {
+  value       = google_compute_instance.app.network_interface[0].access_config[0].nat_ip
+  description = "IP pública de la máquina de aplicación"
+}
+
+output "ip_interna_datos" {
+  value       = google_compute_instance.datos.network_interface[0].network_ip
+  description = "IP interna de la máquina de datos (no pública)"
 }
