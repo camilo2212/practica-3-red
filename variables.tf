3816d0d2aa5a3a1592ca02dd7e07c765d5d1e781
camilo2212 <juancamilouribemendoza@gmail.com>
2026-09-20 17:21:45 -0500

    fase 2: variables y salidas

--- variables.tf
+++ variables.tf
@@ -1,5 +1,8 @@
 # variables.tf
 variable "proyecto"     { type = string }
 variable "prefijo"      { type = string }
-variable "region"       { type = string }
-variable "cidr_publica" { type = string }
+variable "region"       { type = string  default = "us-central1" }
+variable "cidr_publica" { type = string  default = "10.10.1.0/24" }
+variable "zona"          { type = string  default = "us-central1-a" }
+variable "tipo_maquina"  { type = string  default = "e2-micro" }
+variable "cidr_privada"  { type = string  default = "10.10.2.0/24" }
