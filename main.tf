d1004151de747253f6b8fc70d5db661ecb8d1af4
camilo2212 <juancamilouribemendoza@gmail.com>
2026-09-20 18:29:04 -0500

    new main

--- main.tf
+++ main.tf
@@ -10,13 +10,11 @@ provider "google" {
   region  = var.region
 }
 
-# La VPC es global. En modo personalizado nace sin subredes.
 resource "google_compute_network" "vpc" {
   name                    = "${var.prefijo}-vpc"
   auto_create_subnetworks = false
 }
 
-# La subred sí es regional, y es donde las máquinas toman su IP interna.
 resource "google_compute_subnetwork" "publica" {
   name          = "${var.prefijo}-sub-publica"
   ip_cidr_range = var.cidr_publica
@@ -24,7 +22,6 @@ resource "google_compute_subnetwork" "publica" {
   network       = google_compute_network.vpc.id
 }
 
-# Subred de datos: sin salida directa, solo alcanzable desde dentro de la VPC.
 resource "google_compute_subnetwork" "datos" {
   name          = "${var.prefijo}-sub-datos"
   ip_cidr_range = var.cidr_privada
@@ -32,7 +29,25 @@ resource "google_compute_subnetwork" "datos" {
   network       = google_compute_network.vpc.id
 }
 
-# Máquina de aplicación
+resource "google_compute_instance" "datos" {
+  name         = "${var.prefijo}-datos"
+  machine_type = var.tipo_maquina
+  zone         = var.zona
+  tags         = ["datos"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.datos.id
+  }
+
+  metadata_startup_script = file("${path.module}/arranque-datos.sh")
+}
+
 resource "google_compute_instance" "app" {
   name         = "${var.prefijo}-app"
   machine_type = var.tipo_maquina
@@ -50,28 +65,9 @@ resource "google_compute_instance" "app" {
     access_config {}
   }
 
-  metadata_startup_script = file("${path.module}/arranque.sh")
-}
-
-# Máquina de datos: sin access_config, no tiene IP pública.
-resource "google_compute_instance" "datos" {
-  name         = "${var.prefijo}-datos"
-  machine_type = var.tipo_maquina
-  zone         = var.zona
-  tags         = ["datos"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-12"
-    }
-  }
-
-  network_interface {
-    subnetwork = google_compute_subnetwork.datos.id
-    # Sin access_config: no es alcanzable desde internet.
-  }
-
-  metadata_startup_script = file("${path.module}/arranque-datos.sh")
+  metadata_startup_script = templatefile("${path.module}/arranque.sh", {
+    ip_datos = google_compute_instance.datos.network_interface[0].network_ip
+  })
 }
 
 resource "google_compute_firewall" "app_http" {
@@ -96,28 +92,10 @@ resource "google_compute_firewall" "ssh_iap" {
     ports    = ["22"]
   }
 
-  # 35.235.240.0/20 es el rango desde el que Google reenvía SSH
-  # a través de IAP. Es el único origen autorizado para el 22.
   source_ranges = ["35.235.240.0/20"]
   target_tags   = ["app"]
 }
-# Cloud Router: necesario como base para el NAT.
-resource "google_compute_router" "router" {
-  name    = "${var.prefijo}-router"
-  region  = var.region
-  network = google_compute_network.vpc.id
-}
 
-# Cloud NAT: permite que la máquina de datos salga a internet
-# (para instalar paquetes) sin tener IP pública ni ser alcanzable desde afuera.
-resource "google_compute_router_nat" "nat" {
-  name                               = "${var.prefijo}-nat"
-  router                             = google_compute_router.router.name
-  region                             = var.region
-  nat_ip_allocate_option             = "AUTO_ONLY"
-  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
-}
-# Regla interna: solo la app (por etiqueta) puede hablarle a datos.
 resource "google_compute_firewall" "datos_interno" {
   name    = "${var.prefijo}-permitir-datos-interno"
   network = google_compute_network.vpc.name
@@ -129,4 +107,18 @@ resource "google_compute_firewall" "datos_interno" {
 
   source_tags = ["app"]
   target_tags = ["datos"]
+}
+
+resource "google_compute_router" "router" {
+  name    = "${var.prefijo}-router"
+  region  = var.region
+  network = google_compute_network.vpc.id
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.prefijo}-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
