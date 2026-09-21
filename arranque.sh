600a6f7d052bb8c4e57853f0f49d24f9f639fb3c
camilo2212 <juancamilouribemendoza@gmail.com>
2026-09-20 22:46:56 -0500

    fix:arranque.sh

--- arranque.sh
+++ arranque.sh
@@ -1,20 +1,19 @@
-juancamiloum@cloudshell:~/practica-3-red (maquinavirtual-507221)$ git pull
-remote: Enumerating objects: 34, done.
-remote: Counting objects: 100% (30/30), done.
-remote: Compressing objects: 100% (14/14), done.
-remote: Total 22 (delta 12), reused 18 (delta 8), pack-reused 0 (from 0)
-Unpacking objects: 100% (22/22), 3.77 KiB | 33.00 KiB/s, done.
-From https://github.com/camilo2212/practica-3-red
-   f660c57..0ba84b5  main       -> origin/main
-Updating f660c57..0ba84b5
-Fast-forward
- arranque.sh                |  6 ++++++
- practica-mafer/arranque.sh | 47 +++++++++++++++++++++++++++++++++++++++++------
- practica-mafer/datos.sh    |  9 +++++++++
- practica-mafer/main.tf     |  6 ++++--
- practica-mafer/outputs.tf  |  5 +++++
- practica-mafer/reto.tf     | 59 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- 6 files changed, 124 insertions(+), 8 deletions(-)
- create mode 100644 practica-mafer/datos.sh
- create mode 100644 practica-mafer/reto.tf
-juancamiloum@cloudshell:~/practica-3-red (maquinavirtual-507221)$ 
+cat > arranque.sh << 'EOF'
+#!/bin/bash
+apt-get update -y
+apt-get install -y nginx
+INTERNA=$(curl -s -H "Metadata-Flavor: Google" \
+  http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+DATO=$(curl -s -m 5 http://${ip_datos})
+cat > /var/www/html/index.html <<HTML
+<!DOCTYPE html>
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
+<h1>ali - juancamiloum</h1>
+<p>Servidor de aplicación. IP interna: $INTERNA</p>
+<p>Dato desde la máquina privada: $DATO</p>
+</body>
+</html>
+HTML
+EOF
