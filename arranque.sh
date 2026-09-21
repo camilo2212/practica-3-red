48074dfd18cd7d9f6a7b479773a03de747d06d38
camilo2212 <juancamilouribemendoza@gmail.com>
2026-09-20 22:34:19 -0500

    fix: Arranque.sh

--- arranque.sh
+++ arranque.sh
@@ -5,7 +5,13 @@ INTERNA=$(curl -s -H "Metadata-Flavor: Google" \
   http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
 DATO=$(curl -s -m 5 http://${ip_datos})
 cat > /var/www/html/index.html <<HTML
+<!DOCTYPE html>
+<html>
+<head><meta charset="UTF-8"></head>
+<body>
 <h1>ali - juancamiloum</h1>
 <p>Servidor de aplicación. IP interna: $INTERNA</p>
 <p>Dato desde la máquina privada: $DATO</p>
+</body>
+</html>
 HTML
